eef53c89becdda863e1a23174dce6763f06d50b2
JakubSadlowski <76012344+JakubSadlowski@users.noreply.github.com>
2024-12-17 16:26:20 +0100

    Added calculateItemPrice to CheckoutService now need to test for correctness.

--- src/main/java/org/js/checkoutcomponent/service/checkout/service/CheckoutService.java
+++ src/main/java/org/js/checkoutcomponent/service/checkout/service/CheckoutService.java
@@ -1,4 +1,15 @@
 package org.js.checkoutcomponent.service.checkout.service;
 
+import org.js.checkoutcomponent.service.checkout.data.Item;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CheckoutService {
+    private double calculateItemPrice(Item item, int quantity) {
+        int specialPriceGroups = quantity / item.getRequiredQuantity();
+        int remainingItems = quantity % item.getRequiredQuantity();
+
+        return (specialPriceGroups * item.getSpecialPrice() * item.getRequiredQuantity()) +
+                (remainingItems * item.getNormalPrice());
+    }
 }

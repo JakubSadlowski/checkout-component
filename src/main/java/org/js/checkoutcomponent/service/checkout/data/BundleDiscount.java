fd9dd56f1716807d00cc26ee8287b98f92073582
JakubSadlowski <76012344+JakubSadlowski@users.noreply.github.com>
2024-12-17 15:58:07 +0100

    Item and BundleDiscount fields added.

--- src/main/java/org/js/checkoutcomponent/service/checkout/data/BundleDiscount.java
+++ src/main/java/org/js/checkoutcomponent/service/checkout/data/BundleDiscount.java
@@ -1,4 +1,10 @@
 package org.js.checkoutcomponent.service.checkout.data;
 
+import lombok.Data;
+
+@Data
 public class BundleDiscount {
+    private String firstItemID;
+    private String secondItemID;
+    private double discount;
 }
--- src/main/java/org/js/checkoutcomponent/service/checkout/data/Item.java
+++ src/main/java/org/js/checkoutcomponent/service/checkout/data/Item.java
@@ -1,4 +1,11 @@
 package org.js.checkoutcomponent.service.checkout.data;
 
+import lombok.Data;
+
+@Data
 public class Item {
+    private String id;
+    private double normalPrice;
+    private double specialPrice;
+    private int requiredQuantity;
 }

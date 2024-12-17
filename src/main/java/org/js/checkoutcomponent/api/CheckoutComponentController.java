5a412080fd051bf030a5c3dc703678dcba4a695d
JakubSadlowski <76012344+JakubSadlowski@users.noreply.github.com>
2024-12-17 14:42:04 +0100

    First test for CheckoutComponentController passed.

--- src/main/java/org/js/checkoutcomponent/api/CheckoutComponentController.java
+++ src/main/java/org/js/checkoutcomponent/api/CheckoutComponentController.java
@@ -9,7 +9,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.js.checkoutcomponent.model.CheckoutRequest;
 import org.js.checkoutcomponent.model.CheckoutResponse;
-import org.openapitools.model.CalculateTotal400Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -32,9 +31,9 @@ public class CheckoutComponentController {
             @ApiResponse(responseCode = "200", description = "Successful price calculation",
                     content = @Content(mediaType = "application/json",
                             schema = @Schema(implementation = CheckoutResponse.class))),
-            @ApiResponse(responseCode = "400", description = "Invalid input",
+            /*@ApiResponse(responseCode = "400", description = "Invalid input",
                     content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = CalculateTotal400Response.class)))
+                            schema = @Schema(implementation = CalculateTotal400Response.class)))*/
     })
     @PostMapping("/checkout")
     public ResponseEntity<org.js.checkoutcomponent.model.CheckoutResponse> calculateTotal(
--- src/test/java/org/js/checkoutcomponent/api/CheckoutComponentControllerTest.java
+++ src/test/java/org/js/checkoutcomponent/api/CheckoutComponentControllerTest.java
@@ -2,13 +2,11 @@ package org.js.checkoutcomponent.api;
 
 import org.js.checkoutcomponent.model.*;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -16,14 +14,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class CheckoutComponentControllerTest {
     @InjectMocks
     private CheckoutComponentController controller;
-
-    private LocalValidatorFactoryBean validator;
-
-    @BeforeEach
-    void setUp() {
-        validator = new LocalValidatorFactoryBean();
-        validator.afterPropertiesSet();
-    }
 
     @Test
     void calculateTotal_ValidRequest_ReturnsSuccessResponse() {
@@ -37,16 +27,4 @@ class CheckoutComponentControllerTest {
         assertThat(response).isNotNull();
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_IMPLEMENTED);
     }
-
-    /*@Test
-    void calculateTotal_EmptyRequest_ValidationFails() {
-        // Given
-        CheckoutRequest request = new CheckoutRequest();
-
-        // When
-        Set<ConstraintViolation<CheckoutRequest>> violations = validator.validate(request);
-
-        // Then
-        assertFalse(violations.isEmpty());
-    }*/
 }

171519d17752a92f0b7f9a932c1683f035b15d99
JakubSadlowski <76012344+JakubSadlowski@users.noreply.github.com>
2024-12-19 12:12:06 +0100

    1) Added TestRestTemplate to checkoutInvalidRequestWithTwoTheSameItems_returnsBadRequest and test works.

--- src/test/java/org/js/checkoutcomponent/api/CheckoutComponentControllerTest.java
+++ src/test/java/org/js/checkoutcomponent/api/CheckoutComponentControllerTest.java
@@ -9,30 +9,35 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * TODO: Implement such tests
  * 1) Request with empty list of cart items => HttpStatus.OK
- * 2) Request with at least two CartItems in the list having the same ID => HttpStatus.BAD_REQUEST
+ * 2) Request with at least two CartItems in the list having the same ID => HttpStatus.BAD_REQUEST - DONE
  * 3) Request with at least one CartItem having quantity =0 => HttpStatus.BAD_REQUEST
  * 4) Request with at least one CartItem having quantity <0 => HttpStatus.BAD_REQUEST
- * 5) Request with some cart Items which are valid => HttpStatus.OK
+ * 5) Request with some cart Items which are valid => HttpStatus.OK - DONE
  */
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 //@ContextConfiguration(classes = { CheckoutConfig.class, CheckoutComponentController.class })
 @ComponentScan(basePackages = { "org.js.checkoutcomponent.errors" })
-    //@ExtendWith(MockitoExtension.class)
 class CheckoutComponentControllerTest {
     @Autowired
     private CheckoutComponentController controller;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
     void checkoutValidRequest_returnsSuccessResponse() {
@@ -55,21 +60,20 @@ class CheckoutComponentControllerTest {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         CheckoutResponse checkoutResponse = response.getBody();
         Assertions.assertNotNull(checkoutResponse);
-        Assertions.assertEquals(new BigDecimal("230.0"), checkoutResponse.getTotalPrice());
-        Assertions.assertEquals(new BigDecimal("0"), checkoutResponse.getBundleDiscountApplied());
+        assertEquals(new BigDecimal("230.0"), checkoutResponse.getTotalPrice());
+        assertEquals(new BigDecimal("0"), checkoutResponse.getBundleDiscountApplied());
         List<ItemPrice> itemPrices = checkoutResponse.getItemPrices();
-        Assertions.assertEquals(2, itemPrices.size());
+        assertEquals(2, itemPrices.size());
         ItemPrice itemPriceA = itemPrices.get(0);
-        Assertions.assertEquals("A", itemPriceA.getItemId());
-        Assertions.assertEquals(new BigDecimal("170.0"), itemPriceA.getPrice());
-        Assertions.assertEquals(5, itemPriceA.getQuantity());
+        assertEquals("A", itemPriceA.getItemId());
+        assertEquals(new BigDecimal("170.0"), itemPriceA.getPrice());
+        assertEquals(5, itemPriceA.getQuantity());
         ItemPrice itemPriceC = itemPrices.get(1);
-        Assertions.assertEquals("C", itemPriceC.getItemId());
-        Assertions.assertEquals(new BigDecimal("60.0"), itemPriceC.getPrice());
-        Assertions.assertEquals(2, itemPriceC.getQuantity());
+        assertEquals("C", itemPriceC.getItemId());
+        assertEquals(new BigDecimal("60.0"), itemPriceC.getPrice());
+        assertEquals(2, itemPriceC.getQuantity());
     }
 
-    @Disabled("FIXME: Issue with GlobalExceptionHandler")
     @Test
     void checkoutInvalidRequestWithTwoTheSameItems_returnsBadRequest() {
         // Given
@@ -88,11 +92,26 @@ class CheckoutComponentControllerTest {
                 .build()));
 
         // When
-        ResponseEntity<CheckoutResponse> response = controller.calculateTotal(request);
+        //ResponseEntity<CheckoutResponse> response = controller.calculateTotal(request);
+        ResponseEntity<Map> response = restTemplate.postForEntity(
+            "/api/checkout",
+            request,
+            Map.class
+        );
 
         // Then
-        assertThat(response).isNotNull();
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+
+        /*@SuppressWarnings("unchecked")
+        Map<String, Object> body = response.getBody();
+        assertThat(body).isNotNull();
+        assertThat(body.containsKey("message")).isTrue();
+        assertThat(body.containsKey("timestamp")).isTrue();
+        assertThat(body.get("message")).isEqualTo("Some cart items are duplicated");*/
+        /*assertThat(response).isNotNull();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);*/
     }
+
+
 
 }
